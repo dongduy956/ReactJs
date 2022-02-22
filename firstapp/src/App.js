6b1984ca479ef64c6beb8dd3c9d331a88d941136
dongduy956 <dongduy0612@gmail.com>
2022-02-22 18:53:34 +0700

    3_

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Car from './Car.js';
 
 function App() {
   return (
@@ -17,6 +18,8 @@ function App() {
         >
           Learn React
         </a>
+      <Car />
+
       </header>
     </div>
   );
---
+++ firstapp/src/Car.js
@@ -0,0 +1,9 @@
+import React from 'react';
+class Car extends React.Component
+{
+   render(){
+        const html=<h1>Hello word</h1>
+        return html;
+    }
+}
+export default Car;
